6a94c5b31a0a672264ee30ee29c739b9f28b7c88
Lotrando <miroslav.klika@seznam.cz>
2023-11-11 23:01:25 +0100

    add admin to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'last_name'         => 'Admin',
+            'first_name'        => 'Admin',
+            'email'             => 'admin@admin.cz',
+            'email_verified_at' => now(),
+            'password'          => bcrypt('admin'),
+            'remember_token'    => Str::random(10)
+        ]);
+
         User::factory(10)->create();
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,10 +19,10 @@
               Try adjusting your search or filter to find what you're looking for.
             </p>
             <div class="empty-action">
-              <a href="./." class="btn btn-primary">
+              <a class="btn btn-primary" href="./.">
                 <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                  fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                  stroke-linecap="round" stroke-linejoin="round">
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M12 5l0 14" />
                   <path d="M5 12l14 0" />
@@ -38,14 +38,14 @@
           <div class="row align-items-center flex-row-reverse text-center">
             <div class="col-lg-auto ms-lg-auto">
               <ul class="list-inline list-inline-dots mb-0">
-                <li class="list-inline-item"><a href="./docs/" class="link-secondary">Documentation</a></li>
-                <li class="list-inline-item"><a href="./license.html" class="link-secondary">License</a></li>
-                <li class="list-inline-item"><a href="https://github.com/tabler/tabler" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
+                <li class="list-inline-item"><a class="link-secondary" href="./docs/">Documentation</a></li>
+                <li class="list-inline-item"><a class="link-secondary" href="./license.html">License</a></li>
+                <li class="list-inline-item"><a class="link-secondary" href="https://github.com/tabler/tabler" target="_blank" rel="noopener">Source code</a></li>
                 <li class="list-inline-item">
-                  <a href="https://github.com/sponsors/codecalm" target="_blank" class="link-secondary" rel="noopener">
+                  <a class="link-secondary" href="https://github.com/sponsors/codecalm" target="_blank" rel="noopener">
                     <!-- Download SVG icon from http://tabler-icons.io/i/heart -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon text-pink icon-filled icon-inline" width="24" height="24" viewBox="0 0 24 24"
-                      stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg class="icon text-pink icon-filled icon-inline" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                      stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                       <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
                     </svg>
@@ -58,11 +58,11 @@
               <ul class="list-inline list-inline-dots mb-0">
                 <li class="list-inline-item">
                   Copyright &copy; 2023
-                  <a href="." class="link-secondary">Tabler</a>.
+                  <a class="link-secondary" href=".">Tabler</a>.
                   All rights reserved.
                 </li>
                 <li class="list-inline-item">
-                  <a href="./changelog.html" class="link-secondary" rel="noopener">
+                  <a class="link-secondary" href="./changelog.html" rel="noopener">
                     v1.0.0-beta17
                   </a>
                 </li>
